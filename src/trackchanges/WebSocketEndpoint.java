09bcee323097959a39e01062241aed518bee344a
samuel-he <hes@usc.edu>
2018-11-19 20:45:21 -0800

    Update WebSocketEndpoint.java

--- src/trackchanges/WebSocketEndpoint.java
+++ src/trackchanges/WebSocketEndpoint.java
@@ -151,8 +151,8 @@ public class WebSocketEndpoint {
 
 			}else if(request.equals("follow")) {
 				
-				String user_id = (String)json.get("user_id");
-				String follower_id = (String)json.get("follower_id");
+				String user_id = (String)json.get("follower_id");
+				String follower_id = (String)json.get("user_id");
 				System.out.println("1");
 				System.out.println(user_id);
 				System.out.println(follower_id);
@@ -160,7 +160,7 @@ public class WebSocketEndpoint {
 				handleSuccess = app.follow(user_id, follower_id);
 
 			} else if(request.equals("unfollow")) {
-
+				// check logic here
 				String user_id = (String)json.get("user_id");
 				String follower_id = (String)json.get("follower_id");
 				System.out.println("1");

e8a6beac185c2fd95ddc947e7c69d3efd6937002
pjhernandez18 <pjhernan@usc.edu>
2018-11-26 16:59:23 -0800

    PJ: updated backend repo

--- src/trackchanges/WebSocketEndpoint.java
+++ src/trackchanges/WebSocketEndpoint.java
@@ -1,20 +1,11 @@
 package trackchanges;
 
 import java.io.IOException;
-<<<<<<< HEAD
-import java.io.UnsupportedEncodingException;
-import java.nio.ByteBuffer;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-=======
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
->>>>>>> jeff-branch
 import java.util.logging.Logger;
 
 import javax.websocket.OnClose;
@@ -25,121 +16,6 @@ import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import javax.xml.bind.DatatypeConverter;
 
-<<<<<<< HEAD
-import org.json.JSONException;
-import org.json.JSONObject;
-
-
-
-
-
-
-@ServerEndpoint (value="/endpoint")
-public class WebSocketEndpoint {
-	
-	// Store users and their sessions in the map
-	private static final Map<String, Session> sessions = new HashMap<String, Session>();
-	//private static ApplicationLayer application = new ApplicationLayer();
-	private static Lock lock = new ReentrantLock();
-	// Set up logger for debugging
-	private static final Logger log = Logger.getLogger("TrackChanges");
-	
-	
-	//Called when swift client connects
-   @OnOpen
-    public void onOpen(Session session) {
-	lock.lock();
-       System.out.println("onOpen::" + session.getId());        
-	   log.info("Connection openend by id: " + session.getId());
-	  // sessions.put(session.getId(), session);
-	   lock.unlock();
-    }
-   
-  
-	//Called when a connection is closed
-	@OnClose
-	public void close(Session session) {
-		lock.lock();
-		 System.out.println("onClose:: " + session.getId());	
-		 log.info("Connection closed by id: " + session.getId());
-		 lock.unlock();
-	}
-	
-	
-	@OnMessage
-	public void onMessage (byte[] b, Session session) {
-		lock.lock();
-		String printMe="";
-		try {
-			printMe = new String(b, "US-ASCII");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		JSONObject json = null;
-		try {
-			json = new JSONObject(printMe);
-			if(json.get("message").equals("test")){
-				
-				System.out.println("JSON Recieved");
-				
-			}
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-
-		lock.unlock();
-	}
-	
-//  @OnMessage
-//    public void onMessage(String message, Session session) {
-//        System.out.println("onMessage::From=" + session.getId() + " Message=" + message);
-//        
-//        try {
-//            session.getBasicRemote().sendText("Hello Client " + session.getId() + "!");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-  
-//Called when an error for this endpoint occurred
-   @OnError
-    public void onError(Throwable e) {
-       System.out.println("onError::" + e.getMessage());
-
-    }
-   
-   public void sendToSession(Session session, byte[] data) {
-	   
-	   try {
-		   ByteBuffer tobeSent = ByteBuffer.wrap(data);
-		   session.getBasicRemote().sendBinary(tobeSent);
-		   
-	   }catch(IOException ioe) {
-		   sessions.remove(session.getId());   
-	   }
-	   
-   }
-   
-//   public void sendToAllSessions(Session session, byte[] data) {
-//	 
-//	   lock.lock();
-//	   try {
-//		   ByteBuffer tobeSent = ByteBuffer.wrap(data);
-//		   //loop through sessions
-//		   for (String key : sessions.keySet()) {
-//			    if (sessions.get(key)!=session) {
-//			    	sessions.get(key).getBasicRemote().sendBinary(tobeSent);
-//			    }
-//			}		   
-//	   }catch(IOException ioe) {   
-//		   ioe.printStackTrace();
-//	   }
-//	   lock.unlock();
-//	   
-//   }
-   
-  	
-=======
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -441,6 +317,9 @@ public class WebSocketEndpoint {
 				response.put("post_type", post.getPostType());
 				response.put("post_timestamp", post.getPostTimeStamp());
 				response.put("post_user_id", post.getPostUserId());
+				response.put("post_user_displayname", app.getUserDisplayName(post.getPostUserId()));
+				response.put("post_user_imageurl", app.getUserImg(post.getPostUserId()));
+
 				response.put("post_message", post.getPostMessage());
 				response.put("post_song_id", post.getPostSongId());
 				response.put("post_album_id", post.getPostAlbumId());
@@ -607,7 +486,9 @@ public class WebSocketEndpoint {
 			}
 
 		}
-
+		/*
+		 * Function updates the feeds of the user's followers.
+		 */
 		private void updateFeeds(ArrayList<User> followers, JSONObject response) {
 			
 			Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -657,5 +538,4 @@ public class WebSocketEndpoint {
 	}
 
 
->>>>>>> jeff-branch
 }
